bb7fbc91b47160324e9e31006ca1eebff29b00bc
wattie2k1 <wattie2k1@hotmail.com>
2019-03-09 16:25:26 +0000

    first commit

---
+++ config.inc.php
@@ -0,0 +1,5 @@
+<?php
+$e2ip = "192.168.0.1";
+$xmltv_path = "/mypath/e2guide.xml";
+$lang = "de";
+?>
---
+++ e2xmltvgrab.php
@@ -0,0 +1,121 @@
+<?php
+
+if (!file_exists("config.inc.php")) {
+  exit("Failed to open config file!\n");
+  } 
+else {
+  include ("config.inc.php");    
+}
+  
+$channels = new XMLReader;
+if (!$channels->open("channels.xml")) {
+    die("Failed to open 'channels.xml'");
+}
+
+$xw = new XMLWriter();
+$xw->openURI($xmltv_path);
+$xw->setIndent(true);
+$xw->startDocument("1.0", "UTF-8");
+
+$xw->startElement("tv");
+$xw->startAttribute("generator-info-name");
+$xw->text("e2xmltvgrab v0.1 by wattie");
+$xw->endAttribute();
+$xw->startAttribute("generator-info-url");
+$xw->text("http://www.greyhole.de");
+$xw->endAttribute();
+
+while($channels->read()) {
+  if ($channels->nodeType == XMLReader::ELEMENT && $channels->name == 'channel') {
+    $xw->startElement('channel');
+    $xw->startAttribute('id');
+    $xw->text($channels->getAttribute('id'));
+    $xw->endAttribute();
+    
+    $xw->startElement('display-name');
+    $xw->startAttribute('lang');
+    $xw->text($lang);
+    $xw->endAttribute();
+    $xw->text($channels->getAttribute('name'));
+    $xw->endElement(); 
+
+    $xw->startElement('url');
+    $xw->text($channels->getAttribute('url'));
+    $xw->endElement(); 
+
+    $xw->startElement('icon');
+    $xw->startAttribute('src');
+    $xw->text('http://'.$e2ip.'/picon/'.str_replace(":", "_", rtrim($channels->getAttribute('e2id'), ":")).'.png');
+    $xw->endAttribute();
+    $xw->endElement(); 
+    
+    $xw->endElement();         
+    
+    echo "Adding station - ".$channels->getAttribute('name')." - to XMLTV\n";
+    flush();
+  }
+}
+
+$channels->close();
+
+$channels = new XMLReader;
+if (!$channels->open("channels.xml")) {
+    die("Failed to open 'channels.xml'");
+}
+while($channels->read()) {
+  if ($channels->nodeType == XMLReader::ELEMENT && $channels->name == 'channel') {
+      
+    $programme = simplexml_load_file("http://".$e2ip."/web/epgservice?sRef=".$channels->getAttribute('e2id')); 
+   
+    echo "\n\nUpdating epg data for channel: ".$channels->getAttribute('name')."\n";
+    flush();
+    foreach( $programme->xpath( '//e2event' ) as $node ) {
+      echo "o";
+      flush();
+      $xw->startElement('programme');
+      $xw->startAttribute('start');
+      $xw->text(date('YmdHis', (float)$node->e2eventstart) . ' +0000');
+      $xw->endAttribute();
+      $xw->startAttribute('stop');
+      $xw->text(date('YmdHis', (float)$node->e2eventstart+(float)$node->e2eventduration) . ' +0000');
+      $xw->endAttribute();
+      $xw->startAttribute('channel');
+      $xw->text($node->e2eventservicename);
+      $xw->endAttribute();
+      $xw->startElement('title');
+      $xw->startAttribute('lang');
+      $xw->text($lang);
+      $xw->endAttribute();
+      $xw->text($node->e2eventtitle);
+      $xw->endElement();
+      $xw->startElement('sub-title');
+      $xw->startAttribute('lang');
+      $xw->text($lang);
+      $xw->endAttribute();
+      $xw->text($node->e2eventdescription);
+      $xw->endElement();
+      $xw->startElement('desc');
+      $xw->startAttribute('lang');
+      $xw->text($lang);
+      $xw->endAttribute();
+      $xw->text($node->e2eventdescriptionextended);
+      $xw->endElement();
+      $xw->startElement('category');
+      $xw->startAttribute('lang');
+      $xw->text($lang);
+      $xw->endAttribute();
+      $xw->text($node->e2eventdescription);
+      $xw->endElement();
+                  
+      $xw->endElement();
+    }
+  }
+}
+
+$channels->close();
+$xw->endElement();
+
+$xw->endDocument();
+$xw->flush();
+echo "\n\ndone!\n";
+?>
